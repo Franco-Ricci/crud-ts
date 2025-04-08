1c56b79c13fd2c36ba7f1e6ee6b07b35625a337d
franco <francoricci_93@hotmail.com>
2025-04-07 23:59:16 -0300

    creating new user

--- src/components/ListOfUsers.tsx
+++ src/components/ListOfUsers.tsx
@@ -27,9 +27,22 @@ interface User {
 
 export default function ListOfUsers() {
 
-  const [modal, setModal] = useState<boolean>(false);
+  const [modalEdit, setModalEdit] = useState<boolean>(false);
   const [users, setUsers, error] = useUsers();
   const [editingUser, setEditingUser] = useState<User | null>(null)
+
+  const [newUser, setNewUser] = useState<User>({
+    id: Date.now(),
+    name: '',
+    username: '',
+    email: '',
+    website: '',
+    company: {
+      name: '',
+    },
+  
+  })
+  const [modalNewUser, setModalNewUser] = useState<boolean>(false)
 
   // Sync changes to localStorage
   useEffect(() => {
@@ -47,7 +60,7 @@ export default function ListOfUsers() {
   function EditUser(user: User) {
     console.log(user)
     setEditingUser(user);  // Save the selected user
-    setModal(true);
+    setModalEdit(true);
     console.log(editingUser)
   }
 
@@ -58,9 +71,27 @@ export default function ListOfUsers() {
       }
       return e;
     });
+
     setUsers(updatedUsers);
-    setModal(false)
+    setModalEdit(false)
   }
+  function createUser(user: User){
+
+    if(newUser.name.length > 3 && newUser.email !== ""){
+
+      setUsers([...users, newUser]);
+      return // Add new user to users list
+    }
+    
+    setModalNewUser(true);   
+    setNewUser({
+      id: Date.now(),
+      name: '',
+      username: '',
+      email: '',
+      website: '',
+      company: { name: '' }})
+    }
   return (
     <>
       <div className="sm:flex sm:items-center sm:justify-between sm:space-x-10">
@@ -108,10 +139,13 @@ export default function ListOfUsers() {
           ))}
 
         </TableBody>
-
       </Table>
+        <div>
 
-      {modal && editingUser && (
+          <button className='bg-blue-700 hover:bg-blue-600 text-white' onClick={() => {createUser(newUser)}} >Add new user</button>
+        </div>
+
+      {modalEdit && editingUser && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="bg-white p-6 rounded shadow-lg w-[400px]">
             <h2 className="text-lg font-semibold mb-4">Edit User</h2>
@@ -177,7 +211,7 @@ export default function ListOfUsers() {
             <div className="mt-4 flex justify-end">
               <button
                 className="px-4 py-2 bg-gray-300 rounded mr-2"
-                onClick={() => setModal(false)}
+                onClick={() => setModalEdit(false)}
               >
                 Cancel
               </button>
@@ -194,7 +228,93 @@ export default function ListOfUsers() {
           </div>
         </div>
       )}
+      {modalNewUser && 
+     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+     <div className="bg-white p-6 rounded shadow-lg w-[400px]">
+       <h2 className="text-lg font-semibold mb-4">Edit User</h2>
+       <div className="space-y-4">
+         <label htmlFor='name' className='flex items-center gap-3 text-gray-600'>Name
+           <input
+             className="w-full border border-gray-300 rounded px-3 py-2"
+             type="text"
+             value={newUser?.name}
+             onChange={(e) =>
+              setNewUser({ ...newUser, name: e.target.value })
+             }
+             placeholder="Name"
+             required
+           />
+         </label>
+         <label htmlFor='name' className='flex items-center gap-3 text-gray-600'>Username
+         <input
+           className="w-full border border-gray-300 rounded px-3 py-2"
+           type="text"
+           value={newUser?.username}
+           onChange={(e) =>
+            setNewUser({ ...newUser, username: e.target.value })
+           }
+           placeholder="username"
+         />
+         </label>
+         <label htmlFor='email' className='flex items-center gap-3 text-gray-600'>Email
+         <input
+           className="w-full border border-gray-300 rounded px-3 py-2"
+           type="email"
+           value={newUser?.email}
+           onChange={(e) =>
+            setNewUser({ ...newUser, email: e.target.value })
+           }
+           placeholder="email"
+           required
+         />
+         </label>
+         <label htmlFor='website' className='flex items-center gap-3 text-gray-600'>Website
+         <input
+           className="w-full border border-gray-300 rounded px-3 py-2"
+           type="text"
+           value={newUser?.website}
+           onChange={(e) =>
+            setNewUser({ ...newUser, website: e.target.value })
+           }
+           placeholder="website"
+           required
+         />
+          </label>
+          <label htmlFor='Company' className='flex items-center gap-3 text-gray-600'>Company
+         <input
+           className="w-full border border-gray-300 rounded px-3 py-2"
+           type="text"
+           value={newUser?.company.name}
+           onChange={(e) =>
+            setNewUser({ ...newUser, company: { ...newUser.company, name: e.target.value } })
+           }
+           placeholder="Company"
+           required
+         />
+          </label>
 
+       </div>
+
+       <div className="mt-4 flex justify-end">
+         <button
+           className="px-4 py-2 bg-gray-300 rounded mr-2"
+           onClick={() => setModalNewUser(false)}
+         >
+           Cancel
+         </button>
+         <button
+           className="px-4 py-2 bg-blue-600 text-white rounded"
+           onClick={() => {
+             
+             createUser(newUser);
+           }}
+         >
+           Save
+         </button>
+       </div>
+     </div>
+   </div>
+      }
     </>
   );
 }
