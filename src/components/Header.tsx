2e619e1cdaecbcd1ae64b4d01045a06e8fac9d54
franco <francoricci_93@hotmail.com>
2025-04-17 00:51:46 -0300

    responsive

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return ( 
+        <div className="sm:flex sm:items-center sm:justify-center sm:space-x-10">
+        <div>
+          <h3 className="font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
+            Workspaces
+          </h3>
+          <p className="mt-1 text-tremor-default leading-6 text-tremor-content dark:text-dark-tremor-content">
+            Overview of all registered workspaces within your organization.
+          </p>
+        </div>
+
+      </div>    
+    );
+}
--- src/components/ListOfUsers.tsx
+++ src/components/ListOfUsers.tsx
@@ -14,6 +14,7 @@ import {
 } from '@tremor/react';
 import ModalEdit from './ModalEdit';
 import { ModalNewUser } from './ModalNewUser';
+import Header from './Header';
 
 
 export default function ListOfUsers() {
@@ -70,62 +71,52 @@ export default function ListOfUsers() {
   }
   return (
     <>
-      <div className="sm:flex sm:items-center sm:justify-center sm:space-x-10">
-        <div>
-          <h3 className="font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
-            Workspaces
-          </h3>
-          <p className="mt-1 text-tremor-default leading-6 text-tremor-content dark:text-dark-tremor-content">
-            Overview of all registered workspaces within your organization.
-          </p>
-        </div>
+      <Header />
 
-      </div>
+      {loading ? <p className='text-blue-700 text-2xl font-bold'>Loading...</p> :
+        error ? <p className='text-blue-700 text-2xl font-bold'>Error fetching data, try again</p>
+          :
+          <div>
+            <Table className="mt-8 overflow-x-auto">
+              <TableHead>
+                <TableRow className="border-b border-tremor-border dark:border-dark-tremor-border">
+                  <TableHeaderCell>Name</TableHeaderCell>
+                  <TableHeaderCell>Username</TableHeaderCell>
+                  <TableHeaderCell>Email</TableHeaderCell>
+                  <TableHeaderCell className="whitespace-nowrap hidden sm:table-cell">Website</TableHeaderCell>
+                  <TableHeaderCell className="whitespace-nowrap hidden sm:table-cell">Company</TableHeaderCell>
+                  <TableHeaderCell>Edit</TableHeaderCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {users.map((user: User) => (
+                  <TableRow key={user.id}>
+                    <TableCell>{user.name}</TableCell>
+                    <TableCell>{user.username}</TableCell>
+                    <TableCell>{user.email}</TableCell>
+                    <TableCell className="whitespace-nowrap hidden sm:table-cell">{user.website}</TableCell>
+                    <TableCell className="whitespace-nowrap hidden sm:table-cell">{user.company?.name}</TableCell>
+                    <TableCell className='flex flex-wrap gap-2 justify-start'>
+                      <button className="text-blue-600 py-1 px-2 flex items-center gap-1 sm:w-auto w-full " onClick={() => EditUser(user)}><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" > <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /> <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /> <path d="M16 5l3 3" /> </svg> </button>
+                      <button
+                        className="text-red-600 py-1 px-2 flex items-center gap-1 sm:w-auto w-full"
+                        onClick={() => removeUser(user.id)}
+                      >
+                        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" > <path d="M18 6l-12 12" /> <path d="M6 6l12 12" /> </svg>
 
-{loading ? <p className='text-blue-700 text-2xl font-bold'>Loading...</p> :
-      error ? <p className='text-blue-700 text-2xl font-bold'>Error fetching data, try again</p> 
-      : 
-      <div className='overflow-x-auto'>
-      <Table className="mt-8 ">
-        <TableHead>
-          <TableRow className="border-b border-tremor-border dark:border-dark-tremor-border">
-            <TableHeaderCell>Name</TableHeaderCell>
-            <TableHeaderCell>Username</TableHeaderCell>
-            <TableHeaderCell>Email</TableHeaderCell>
-            <TableHeaderCell>Website</TableHeaderCell>
-            <TableHeaderCell>Company</TableHeaderCell>
-            <TableHeaderCell>Edit</TableHeaderCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users.map((user: User) => (
-            <TableRow key={user.id}>
-              <TableCell>{user.name}</TableCell>
-              <TableCell>{user.username}</TableCell>
-              <TableCell>{user.email}</TableCell>
-              <TableCell>{user.website}</TableCell>
-              <TableCell>{user.company?.name}</TableCell>
-              <TableCell>
-                <button className="text-blue-600" onClick={() => EditUser(user)}><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" > <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /> <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /> <path d="M16 5l3 3" /> </svg> </button>
-                <button
-                  className="text-red-600 ml-2"
-                  onClick={() => removeUser(user.id)}
-                >
-                  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" > <path d="M18 6l-12 12" /> <path d="M6 6l12 12" /> </svg> 
+                      </button>
+                    </TableCell>
+                  </TableRow>
+                ))}
 
-                </button>
-              </TableCell>
-            </TableRow>
-          ))}
-
-        </TableBody>
-      </Table>
+              </TableBody>
+            </Table>
+          </div>
+      }
       <div>
 
         <button className='bg-blue-700 hover:bg-blue-600 text-white' onClick={() => { setModalNewUser(true); }} >Add new user</button>
       </div>
-      </div>
-    }
       {modalEdit && editingUser && (
         <ModalEdit
           editingUser={editingUser}
